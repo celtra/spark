c82e7f98c638c6930a2a2b3cdf9f3a2c35b2333b
Viljem Skornik <vili@celtra.com>
2013-04-13 20:38:55 +0000

    Autocreate ami if needed based on git hash, as it used to be.

--- lib/modules/cluster-create.sh
+++ lib/modules/cluster-create.sh
@@ -8,10 +8,34 @@ source $APP_ROOT/lib/common.sh
 MODULE_NAME="create"
 MODULE_DESCRIPTION="Create Cluster with Amazon CloutFormation Template"
 
-PARAMETERS=$(aws_generate_parameters_json)
 TEMPLATE=$($APP_ROOT/lib/stack-template)
 
+TMP_DIR=/tmp/$(basename $0).$$
+mkdir -p $TMP_DIR
+cleanup() {
+    rm -fr $TMP_DIR
+}
+trap cleanup EXIT
+
 echo "Creating cluster $CFN_CLUSTER_NAME"
+
+# Local AMI Hash
+AMI_HASH_ID=$($APP_ROOT/ami/base-ami-hash)
+# Currently deployed AMI_ID
+AMI_BUCKET_ID=$(aws ec2 describe-images --filters "[ { \"name\": \"is-public\", \"values\": [ \"false\" ] },{ \"name\": \"name\", \"values\": [ \"spark-0.6.2-${AMI_HASH_ID}\" ] } ]" | \
+                $APP_ROOT/lib/jsawk -n 'if (typeof this.Images[0] != "undefined") { out(this.Images[0].ImageId); }')
+
+if [ "$AMI_BUCKET_ID" == "" ]
+then
+    sudo env PATH=$PATH $APP_ROOT/ami/build-ami -b -B $S3_AMI_BUCKET -o $TMP_DIR/ami.id
+    AMI_ID=$(cat $TMP_DIR/ami.id | awk '{ print $2; }')
+else
+    AMI_ID=$AMI_BUCKET_ID
+fi
+
+CFN_PARAMS['ami']=$AMI_ID
+PARAMETERS=$(aws_generate_parameters_json)
+
 if OUTPUT=$(aws cloudformation create-stack --template-body "$TEMPLATE" --parameters "${PARAMETERS}" --stack-name $CFN_CLUSTER_NAME)
 then
     aws_cfn_wait_for_quit_status "CREATE_COMPLETE" "ROLLBACK_COMPLETE"
