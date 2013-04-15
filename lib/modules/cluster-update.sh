6aa9b3a52a6cf3ce0062a1ffe399a72d9391680f
Viljem Skornik <vili@celtra.com>
2013-04-15 10:01:57 +0000

    cluster-update now supports ami rebuild.

--- lib/modules/cluster-update.sh
+++ lib/modules/cluster-update.sh
@@ -12,6 +12,27 @@ TEMPLATE=$($APP_ROOT/lib/stack-template)
 PARAMETERS=$(aws_generate_parameters_json)
 
 echo "Updating cluster $CFN_CLUSTER_NAME"
-aws cloudformation update-stack --template-body "$TEMPLATE" --parameters "${PARAMETERS}" --stack-name $CFN_CLUSTER_NAME >/dev/null
 
-aws_cfn_wait_for_quit_status "UPDATE_COMPLETE" "ROLLBACK_COMPLETE"
+# Local AMI Hash
+AMI_HASH_ID=$($APP_ROOT/ami/base-ami-hash)
+# Currently deployed AMI_ID
+AMI_BUCKET_ID=$(aws ec2 describe-images --filters "[ { \"name\": \"is-public\", \"values\": [ \"false\" ] },{ \"name\": \"name\", \"values\": [ \"spark-0.6.2-${AMI_HASH_ID}\" ] } ]" | \
+                $APP_ROOT/lib/jsawk -n 'if (typeof this.Images[0] != "undefined") { out(this.Images[0].ImageId); }')
+
+if [ "$AMI_BUCKET_ID" == "" ]
+then
+    sudo env PATH=$PATH $APP_ROOT/ami/build-ami -b -B $S3_AMI_BUCKET -o $TMP_DIR/ami.id
+    AMI_ID=$(cat $TMP_DIR/ami.id | awk '{ print $2; }')
+else
+    AMI_ID=$AMI_BUCKET_ID
+fi
+
+CFN_PARAMS['ami']=$AMI_ID
+PARAMETERS=$(aws_generate_parameters_json)
+
+if OUTPUT=$(aws cloudformation update-stack --template-body "$TEMPLATE" --parameters "${PARAMETERS}" --stack-name $CFN_CLUSTER_NAME)
+then
+    aws_cfn_wait_for_quit_status "UPDATE_COMPLETE" "ROLLBACK_COMPLETE"
+else
+    error "$OUTPUT"
+fi
