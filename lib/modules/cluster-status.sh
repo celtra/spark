8e62aa0ea4714653623acf05e1d4446eeb61c94d
Viljem Skornik <vili@celtra.com>
2013-04-15 20:34:24 +0000

    Status fix

--- lib/modules/cluster-status.sh
+++ lib/modules/cluster-status.sh
@@ -10,10 +10,14 @@ MODULE_DESCRIPTION="Display various Cluster stats"
 
 MASTER_URL=$(aws_get_instances "Master" | awk '{print $2}')
 SLAVE_COUNT=$(aws_get_instances "Slave" | wc -l)
-SLAVE_RUNNING=$($APP_ROOT/bin/cluster run -q -r Slave "pgrep -fc 'java.*Worker'"  | wc -l)
+SLAVE_RUNNING=0
+for i in $($APP_ROOT/bin/cluster run -q -r Slave "pgrep -fc 'java.*Worker'")
+do
+    SLAVE_RUNNING=$(( $SLAVE_RUNNING + $i))
+done
 echo "Spark Cluster $CFN_CLUSTER_NAME"
 echo "==========================="
-echo "Cluster Status:  "$(aws_get_stack_status)
+echo "Cluster Status:  $(aws_get_stack_status)"
 echo "Master URL:      http://${MASTER_URL}:8080"
 echo "Master Endpoint: spark://${MASTER_URL}:7077"
 echo "Slave Nodes:     $SLAVE_RUNNING/$SLAVE_COUNT"
