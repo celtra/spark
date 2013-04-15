8e9a6cfcc057bec29cfa0a0e9e0da6227c796542
Viljem Skornik <vili@celtra.com>
2013-04-15 21:46:43 +0000

    Add node utilization

--- lib/modules/cluster-status.sh
+++ lib/modules/cluster-status.sh
@@ -10,16 +10,25 @@ MODULE_DESCRIPTION="Display various Cluster stats"
 
 MASTER_URL=$(aws_get_instances "Master" | awk '{print $2}')
 SLAVE_COUNT=$(aws_get_instances "Slave" | wc -l)
+
 SLAVE_RUNNING=0
 for i in $($APP_ROOT/bin/cluster run -q -r Slave "pgrep -fc 'java.*Worker'")
 do
     SLAVE_RUNNING=$(( $SLAVE_RUNNING + $i))
 done
+
+SLAVE_UTILIZATION=0
+for i in $($APP_ROOT/bin/cluster run -q -r Slave 'pgrep -cf java.*Standalone')
+do
+    SLAVE_UTILIZATION=$(( $SLAVE_UTILIZATION + $i))
+done
+
 echo "Spark Cluster $CFN_CLUSTER_NAME"
 echo "==========================="
-echo "Cluster Status:  $(aws_get_stack_status)"
-echo "Master URL:      http://${MASTER_URL}:8080"
-echo "Master Endpoint: spark://${MASTER_URL}:7077"
-echo "Slave Nodes:     $SLAVE_RUNNING/$SLAVE_COUNT"
+echo "Cluster Status:    $(aws_get_stack_status)"
+echo "Master URL:        http://${MASTER_URL}:8080"
+echo "Master Endpoint:   spark://${MASTER_URL}:7077"
+echo "Slave Nodes:       $SLAVE_RUNNING/$SLAVE_COUNT"
+echo "Slave Utilization: $SLAVE_UTILIZATION/$SLAVE_COUNT"
 #echo "Slave CPUs:  $SLAVE_CPUS"
 #echo "Slave MEM:   $SLAVE_MEM"
