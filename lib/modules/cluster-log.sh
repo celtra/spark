ba3b0962b407f7822c335fffe5937911c2a032b9
Viljem Skornik <vili@celtra.com>
2013-04-16 17:29:28 +0000

    Just tail relevant logs

--- lib/modules/cluster-log.sh
+++ lib/modules/cluster-log.sh
@@ -7,4 +7,4 @@ source $APP_ROOT/config/config.sh
 MODULE_NAME="log"
 MODULE_DESCRIPTION="Tail all cluster logs"
 
-$APP_ROOT/lib/modules/cluster-run.sh tail -f /tmp/spark-work/*/*/std* /opt/spark/logs/*
+$APP_ROOT/lib/modules/cluster-run.sh tail -f 'tail -f /tmp/spark-work/$(ls -1 /tmp/spark-work/ | tail -n1)/*/std* /opt/spark/logs/*.out'
